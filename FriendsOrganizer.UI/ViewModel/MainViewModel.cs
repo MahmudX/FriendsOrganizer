941e954195bd343884141400b9c5049553d0acf8
Mahmudul Hasan <Mahmudulhasan1999@outlook.com>
2021-05-18 14:28:23 +0600

    Added MainViewModel and inherited it from ViewModelBase.

---
+++ FriendsOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using FriendsOrganizer.Model;
+using FriendsOrganizer.UI.Data;
+
+namespace FriendsOrganizer.UI.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IFriendDataService friendDataService;
+        private Friend selectedFriend;
+
+        public ObservableCollection<Friend> Friends { get; set; }
+
+        public Friend SelectedFriend
+        {
+            get => selectedFriend;
+            set
+            {
+                selectedFriend = value; 
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            Friends = new ObservableCollection<Friend>();
+            this.friendDataService = friendDataService;
+        }
+
+        public void Load()
+        {
+            Friends.Clear();
+            foreach (var friend in friendDataService.GetAll())
+            {
+                Friends.Add(friend);
+            }
+        }
+
+
+    }
+}
