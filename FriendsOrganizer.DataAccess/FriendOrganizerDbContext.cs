ed2eaec9526abd04975303cc4eea6e855de5dc8c
Mahmudul Hasan <Mahmudulhasan1999@outlook.com>
2021-05-18 18:02:59 +0600

    Added EntityFrameworkCore.

---
+++ FriendsOrganizer.DataAccess/FriendOrganizerDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using FriendsOrganizer.Model;
+using Microsoft.EntityFrameworkCore;
+namespace FriendsOrganizer.DataAccess
+{
+    public class FriendOrganizerDbContext : DbContext
+    {
+        public DbSet<Friend> Friends { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Friend>().HasData(new List<Friend>
+            {
+                new() { Id = Guid.NewGuid(), FirstName = "Mahmudul", LastName = "Hasan" },
+                new() { Id = Guid.NewGuid(), FirstName = "Ariful", LastName = "Islam" },
+                new () { Id = Guid.NewGuid(), FirstName = "Zakir", LastName = "Hossen" },
+                new () { Id = Guid.NewGuid(), FirstName = "Ashiqur", LastName = "Rahman" }
+            });
+
+        }
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=FriendOrganization;Trusted_Connection=True;MultipleActiveResultSets=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- FriendsOrganizer.Model/Friend.cs
+++ FriendsOrganizer.Model/Friend.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace FriendsOrganizer.Model
 {
     public class Friend
     {
+        [Key]
         public Guid Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50)]
         public string Email { get; set; }
     }
 }
